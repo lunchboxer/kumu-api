1088080a34b00755d0d791eb75253c9b11688f67
lunchboxer <zixingchepeng@163.com>
2016-02-05 17:27:19 +0800

    Add skeleton angular app which does nothing

---
+++ client/js/app.js
@@ -0,0 +1,16 @@
+angular
+  .module('app', [
+    'lbServices',
+    'ui.router'
+  ])
+  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider,
+      $urlRouterProvider) {
+    $stateProvider
+      .state('welcome', {
+        url: '',
+        templateUrl: 'views/welcome.html',
+        controller: 'TodoController'
+      });
+
+    $urlRouterProvider.otherwise('welcome');
+  }]);
