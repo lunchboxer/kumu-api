3638be2f879fd78a6ab2feac55512a6665688025
lunchboxer <zixingchepeng@163.com>
2016-02-13 18:06:07 +0800

    Add basic user auth. shrink logo. grow heading

--- client/js/app.js
+++ client/js/app.js
@@ -7,9 +7,36 @@ angular
       $urlRouterProvider) {
     $stateProvider
       .state('welcome', {
-        url: '',
+        url: '/welcome',
         templateUrl: 'views/welcome.html'
-      });
-
+      })
+      .state('login', {
+        url: '/login',
+        templateUrl: 'views/login.html',
+        controller: 'AuthLoginController'
+      })
+      .state('logout', {
+        url: '/logout',
+        controller: 'AuthLogoutController'
+      })
+      .state('forbidden', {
+        url: '/forbidden',
+        templateUrl: 'views/forbidden.html',
+      })
+      .state('dashboard', {
+        url: '/dashboard',
+        templateUrl: 'views/dashboard.html',
+        // controller: 'DashboardController'
+        authenticate: true
+      })
     $urlRouterProvider.otherwise('welcome');
+  }])
+  .run(['$rootScope', '$state', function($rootScope, $state) {
+    $rootScope.$on('$stateChangeStart', function(event, next) {
+      // redirect to login page if not logged in
+      if (next.authenticate && !$rootScope.currentUser) {
+        event.preventDefault(); //prevent current page from loading
+        $state.go('forbidden');
+      }
+    });
   }]);
---
+++ client/js/services/auth.js
@@ -0,0 +1,42 @@
+angular
+  .module('app')
+  .factory('AuthService', ['Teacher', '$q', '$rootScope', function(User, $q,
+      $rootScope) {
+    function login(email, password) {
+      return User
+        .login({email: email, password: password})
+        .$promise
+        .then(function(response) {
+          $rootScope.currentUser = {
+            id: response.user.id,
+            tokenId: response.id,
+            email: email,
+            username: response.user.username
+          };
+        });
+    }
+
+    function logout() {
+      return User
+       .logout()
+       .$promise
+       .then(function() {
+         $rootScope.currentUser = null;
+       });
+    }
+
+    function register(email, password) {
+      return User
+        .create({
+         email: email,
+         password: password
+       })
+       .$promise;
+    }
+
+    return {
+      login: login,
+      logout: logout,
+      register: register
+    };
+  }]);
