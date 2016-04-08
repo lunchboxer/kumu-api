a1c126a107f933ed76204f37d170e52597b91d45
lunchboxer <zixingchepeng@163.com>
2016-04-09 04:40:36 +0800

    add gulp, simplify build, add static index

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ In it's current development state, it is a loopback installation with data struc
 ## Angular 2
 
 This branch is for setting up a foundation using angular 2 instead. It will emulate the methods used in [Qeti project](https://github.com/Qeti/Qeti). And explained in this [StackOverflow question](http://stackoverflow.com/questions/34843235/is-it-possible-to-generate-services-for-angular2-from-loopback).
+
+## Install
+
+Only source files are commited to the repo, so compilation and copying will need to be done before it can be used. In particular lb-ng will complain if favicons aren't copied over first.
---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp       = require('gulp'),
+    webserver = require('gulp-webserver'),
+    watch      = require('gulp-watch');
+
+gulp.task('copylibs', function() {
+  return gulp
+    .src([
+      'node_modules/es6-shim/es6-shim.min.js',
+      'node_modules/systemjs/dist/system-polyfills.js',
+      'node_modules/angular2/bundles/angular2-polyfills.js',
+      'node_modules/systemjs/dist/system.src.js',
+      'node_modules/rxjs/bundles/Rx.js',
+      'node_modules/angular2/bundles/angular2.dev.js'
+    ])
+    .pipe(gulp.dest('client/lib'));
+});
+
+gulp.task('webserver', function() {
+  gulp.src('client/build')
+    .pipe(webserver({
+      livereload: true,
+      open: true
+    }));
+});
+
+gulp.task('default', ['webserver']);
