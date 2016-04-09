a784d67771655efd6910a2fb0f158b7b320072ac
lunchboxer <zixingchepeng@163.com>
2016-04-09 17:51:58 +0800

    Build simple working angular2 page

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+ ///<reference path="../../../node_modules/angular2/typings/browser.d.ts"/>
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'templates/app.html'
+})
+export class AppComponent {
+
+}
