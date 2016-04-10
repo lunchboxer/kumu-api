70c360b18cb8ae1e9ce1b31a2fe8c6fc63d9848a
lunchboxer <zixingchepeng@163.com>
2016-04-10 11:42:06 +0800

    Add http module (not actually used yet)

--- client/src/app/students.component.ts
+++ client/src/app/students.component.ts
@@ -1,11 +1,26 @@
  ///<reference path="../../../node_modules/angular2/typings/browser.d.ts"/>
-import { Component } from 'angular2/core';
+import { Component, Injectable, OnInit } from 'angular2/core';
 import { Router } from 'angular2/router';
+import { StudentApi as StudentService } from './lb-services';
+import { HTTP_PROVIDERS } from 'angular2/http';
 
 @Component ({
   selector: 'students',
-  template: '<h2>Students</h2>'
+  template: '<h2>Students</h2>',
+  providers: [ StudentService, HTTP_PROVIDERS],
+
 })
-export class StudentsComponent {
-  constructor(private _router: Router) {}
+@Injectable()
+export class StudentsComponent implements OnInit {
+  ngOnInit(){
+    this.getStudents();
+  }
+  students: any
+
+  constructor(private _router: Router,
+              private _studentService: StudentService ) {}
+  getStudents(){
+    this.students = this._studentService.find();
+    console.log(this.students);
+  }
 }
--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,8 @@ gulp.task('copylibs', function() {
       'node_modules/systemjs/dist/system.src.js',
       'node_modules/rxjs/bundles/Rx.js',
       'node_modules/angular2/bundles/angular2.dev.js',
-      'node_modules/angular2/bundles/router.dev.js'
+      'node_modules/angular2/bundles/router.dev.js',
+      'node_modules/angular2/bundles/http.dev.js'
     ])
     .pipe(gulp.dest('client/build/lib/js'));
 });
