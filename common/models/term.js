7cffdaf8c19db2622d2841f3f813f874978e52c7
lunchboxer <zixingchepeng@163.com>
2016-04-17 21:03:49 +0800

    Add basic validation to term model, end after begin.

--- common/models/term.js
+++ common/models/term.js
@@ -1,3 +1,21 @@
 module.exports = function(Term) {
+  // Make sure the time parts are zero
+  Term.observe('before save', function atMidnight(ctx, next) {
+    if (ctx.instance) {
+      ctx.instance.beginDate.setHours(0,0,0,0);
+      ctx.instance.endDate.setHours(0,0,0,0);
+    } else {
+      ctx.data.beginDate.setHours(0,0,0,0);
+      ctx.data.endDate.setHours(0,0,0,0);
+    }
+    next();
+  });
+
+  Term.validate('endDate', afterBegin, {message: 'End date must be after begin date.'})
+  function afterBegin(err) {
+    console.log("endDate : " + this.endDate)
+    console.log("beginDate : " + this.beginDate)
+    if(this.endDate <= this.beginDate) err();
+  }
 
 };
